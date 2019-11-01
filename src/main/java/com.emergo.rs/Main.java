ed3378671161a2765a5c7e34c0943a9001aaf2ba
83511@global.ul.com <Hugo#2019>
2019-11-01 14:34:44 -0500

    Modified the main method

--- src/main/java/com.emergo.rs/Main.java
+++ src/main/java/com.emergo.rs/Main.java
@@ -4,6 +4,8 @@ package com.emergo.rs;
 public class Main{
 
     public static void main(String args[]){
+
         System.out.println("TEST");
+        System.out.println("This is the second change");
     }
 }
