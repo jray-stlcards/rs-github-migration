d97d2a3a957c8326169d9d9b4e1ce61fa2cafc0e
83511@global.ul.com <Hugo#2019>
2019-11-01 14:38:18 -0500

    Refactored main method

--- src/main/java/com.emergo.rs/Main.java
+++ src/main/java/com.emergo.rs/Main.java
@@ -4,7 +4,10 @@ package com.emergo.rs;
 public class Main{
 
     public static void main(String args[]){
+        print();
+    }
 
+    public static void print(){
         System.out.println("TEST");
         System.out.println("This is the second change");
     }
